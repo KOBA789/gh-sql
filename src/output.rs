b7eaf2fcc0d4bf76042485c9d6861a1f6e9f2a65
Hidekazu Kobayashi <kobahide789@gmail.com>
2021-12-22 17:59:44 +0900

    Give up use of column command

--- src/output.rs
+++ src/output.rs
@@ -1,11 +1,8 @@
-use std::{
-    io::{BufWriter, Write},
-    process::{Command, Stdio},
-    str::FromStr,
-};
+use std::{fmt, io, str::FromStr};
 
 use anyhow::{anyhow, Error, Result};
 use gluesql::prelude::Value;
+use unicode_width::UnicodeWidthStr;
 
 #[derive(Debug)]
 pub enum Format {
@@ -26,7 +23,12 @@ impl FromStr for Format {
 }
 
 impl Format {
-    pub fn print<W: Write>(&self, w: W, labels: Vec<String>, rows: Vec<Vec<Value>>) -> Result<()> {
+    pub fn print<W: io::Write>(
+        &self,
+        w: W,
+        labels: Vec<String>,
+        rows: Vec<Vec<Value>>,
+    ) -> Result<()> {
         match self {
             Format::Table => print_as_table(w, labels, rows),
             Format::Json => print_as_json(w, labels, rows),
@@ -34,62 +36,73 @@ impl Format {
     }
 }
 
-fn print_as_table<W: Write>(mut w: W, labels: Vec<String>, rows: Vec<Vec<Value>>) -> Result<()> {
-    let mut column = Command::new("column")
-        .args(["-s", "\t", "-n", "-t"])
-        .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
-        .spawn()?;
-    let pipe = column.stdin.as_mut().expect("stdin is piped");
-    let mut pipe = BufWriter::new(pipe);
-    pipe.write_all(b"| ")?;
-    for label in labels {
-        pipe.write_all(label.as_bytes())?;
-        pipe.write_all(b"\t| ")?;
-    }
-    pipe.write_all(b"\n")?;
-    for row in rows {
-        pipe.write_all(b"| ")?;
-        for value in row {
-            print_value_in_table(&mut pipe, &value)?;
-            pipe.write_all(b"\t| ")?;
+fn print_as_table<W: io::Write>(
+    mut w: W,
+    labels: Vec<String>,
+    rows: Vec<Vec<Value>>,
+) -> Result<()> {
+    let rows = rows
+        .into_iter()
+        .map(|row| {
+            row.into_iter()
+                .map(|value| {
+                    let mut s = String::new();
+                    print_value_in_table(&mut s, &value)?;
+                    let width = s.width();
+                    Ok((s, width))
+                })
+                .collect::<Result<Vec<_>>>()
+        })
+        .collect::<Result<Vec<_>>>()?;
+    let label_widths = labels.iter().map(|label| label.width()).collect::<Vec<_>>();
+    let column_widths = rows.iter().fold(label_widths, |mut widths, row| {
+        for (max_width, (_, width)) in widths.iter_mut().zip(row) {
+            *max_width = (*max_width).max(*width);
         }
-        pipe.write_all(b"\n")?;
+        widths
+    });
+    w.write_all(b"| ")?;
+    for (max_width, label) in column_widths.iter().zip(labels) {
+        write!(w, "{}{:pad$} | ", label, "", pad = max_width - label.width())?;
     }
-    pipe.flush()?;
-    drop(pipe);
-    let output = column.wait_with_output()?;
-    w.write_all(&output.stdout)?;
+    w.write_all(b"\n")?;
+    for row in rows {
+        w.write_all(b"| ")?;
+        for (max_width, (value, width)) in column_widths.iter().zip(row) {
+            write!(w, "{}{:pad$} | ", value, "", pad = max_width - width)?;
+        }
+        w.write_all(b"\n")?;
+    }
     Ok(())
 }
 
-fn print_value_in_table<W: Write>(w: &mut W, value: &Value) -> std::io::Result<()> {
+fn print_value_in_table<W: fmt::Write>(fmt: &mut W, value: &Value) -> Result<(), fmt::Error> {
     match value {
-        Value::Bool(b) => write!(w, "{}", *b),
-        Value::I64(i) => write!(w, "{}", *i),
-        Value::F64(f) => write!(w, "{}", *f),
-        Value::Str(s) => write!(w, "{}", s),
-        Value::Date(dt) => write!(w, "{}", *dt),
-        Value::Timestamp(ts) => write!(w, "{}", *ts),
-        Value::Time(tm) => write!(w, "{}", *tm),
+        Value::Bool(b) => write!(fmt, "{}", *b),
+        Value::I64(i) => write!(fmt, "{}", *i),
+        Value::F64(f) => write!(fmt, "{}", *f),
+        Value::Str(s) => write!(fmt, "{}", s),
+        Value::Date(dt) => write!(fmt, "{}", *dt),
+        Value::Timestamp(ts) => write!(fmt, "{}", *ts),
+        Value::Time(tm) => write!(fmt, "{}", *tm),
         Value::Interval(_) => unimplemented!(),
         Value::Uuid(_) => unimplemented!(),
         Value::Map(_) => unimplemented!(),
         Value::List(list) => {
             if let [head, tail @ ..] = list.as_slice() {
-                print_value_in_table(w, head)?;
+                print_value_in_table(fmt, head)?;
                 for elem in tail {
-                    write!(w, ", ")?;
-                    print_value_in_table(w, elem)?;
+                    write!(fmt, ", ")?;
+                    print_value_in_table(fmt, elem)?;
                 }
             }
             Ok(())
         }
-        Value::Null => write!(w, ""),
+        Value::Null => write!(fmt, ""),
     }
 }
 
-fn print_as_json<W: Write>(mut w: W, labels: Vec<String>, rows: Vec<Vec<Value>>) -> Result<()> {
+fn print_as_json<W: io::Write>(mut w: W, labels: Vec<String>, rows: Vec<Vec<Value>>) -> Result<()> {
     for row in rows {
         let mut row_map = serde_json::Map::with_capacity(labels.len());
         for (label, value) in labels.iter().zip(row) {
