bbe711c8a91ebc58014c42f41f3652783830d065
ylides <e.propagating@gmail.com>
2023-04-12 16:27:32 +0900

    Revert "Download schema file on build with caching"
    This reverts commit 67c63564f71996e690bfe5f07d948d0909807fe0.

--- build.rs
+++ build.rs
@@ -12,17 +12,8 @@ use graphql_client_codegen::{
 use syn::Token;
 
 fn main() {
-    let schema_path = format!("{}/schema.docs.graphql", env::var("OUT_DIR").unwrap());
-    let schema_path = Path::new(&schema_path);
-    if !schema_path.exists() {
-        File::create(schema_path).unwrap();
-        let body = reqwest::blocking::get("https://docs.github.com/public/schema.docs.graphql")
-            .unwrap()
-            .text()
-            .unwrap();
-        std::fs::write(schema_path, body).unwrap();
-    }
-
+    // download it from https://docs.github.com/public/schema.docs.graphql
+    let schema_path = "schema.docs.graphql".to_string();
     for file_name in [
         "delete_item",
         "list_items",
@@ -38,7 +29,7 @@ fn main() {
         );
         let gen = generate_module_token_stream(
             format!("src/{file_name}.graphql").into(),
-            Path::new(schema_path),
+            Path::new(&schema_path),
             options,
         )
         .unwrap();
@@ -54,9 +45,9 @@ fn main() {
             .arg(dest_file_path)
             .output()
             .unwrap();
+        io::stderr().write_all(&output.stderr).unwrap();
         let status = output.status;
         if !status.success() {
-            io::stderr().write_all(&output.stderr).unwrap();
             panic!("rustfmt error: {status}")
         }
     }
